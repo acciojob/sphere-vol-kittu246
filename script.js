f5b166d6dff0cf0a7a5cbc68358655af393c7661
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-21 14:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function volume_sphere() {
     //Write your code here
-  console.log(form.radius.value);
+  // console.log(form.radius.value);
   let radius = form.radius.value;
   form.volume.value = (4/3)*Math.PI*(radius**3);
-  console.log(form.volume.value);
+  // console.log(form.volume.value);
 } 
 let form = document.getElementById('MyForm');
 form.addEventListener("submit",volume_sphere);
